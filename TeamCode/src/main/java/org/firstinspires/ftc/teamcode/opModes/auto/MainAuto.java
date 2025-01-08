97ea936bc292d6fa022fdb9ceb9db372d97f4a61
ChickenLorde <shmoopthing@gmail.com>
2025-01-08 16:27:41 -0600

    Renamed MainAuto to OldAuto Renamed NewMethodsAuto to MainAuto
    Added comments listing each step of the auto

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opModes/auto/MainAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opModes/auto/MainAuto.java
@@ -5,76 +5,132 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
 import org.firstinspires.ftc.teamcode.subsystems.Robot;
 
-@Autonomous(name = "MainAuto")
+@Autonomous(name = "NewMethodsAuto")
 public class MainAuto extends LinearOpMode{
-    public void waitSeconds(double seconds) {
-        long startTime = System.currentTimeMillis();
-        while(true)
-            if (System.currentTimeMillis() - startTime >= seconds * 1000)
-                return;
-    }
 
     public void runOpMode() throws InterruptedException {
         Robot bot = new Robot(hardwareMap, telemetry);
         //Drive drive = new Drive(hardwareMap, telemetry);
+        bot.setArmPos(-750);
         while(!isStarted()) {
-            bot.arm.setPosition(-750);
-        }
-        long startTime = System.currentTimeMillis();
-        while(System.currentTimeMillis() - startTime < 1.5 * 1000) {
-            bot.arm.setPosition(-400);
+            bot.holdPosition();
         }
 
-        bot.drive.toVectorOld(0, 900);
+        //FIRST BLOCK
+            bot.setArmPos(-400);
 
+            bot.drive.toVectorOld(0, 900);
 
-        waitSeconds(1);
+            bot.waitSeconds(1);
 
-        startTime = System.currentTimeMillis();
-        while(System.currentTimeMillis() - startTime < 3 * 1000) {
-            bot.arm.setPosition(-280);
-            bot.slides.setPosition(950, 0.5);
-        }    
-        /*startTime = System.currentTimeMillis();
-        while(System.currentTimeMillis() - startTime < 2 * 1000) {
-            bot.arm.setPosition(1000);
-            bot.slides.setPosition(700, 0.5);]
-        }*/
-        startTime = System.currentTimeMillis();
-        while(System.currentTimeMillis() - startTime < 1 * 1000) {
-            bot.arm.setPosition(-340);
-            bot.slides.setPosition(950, 0.5);
-        }
-        startTime = System.currentTimeMillis();
-        while(System.currentTimeMillis() - startTime < 2 * 1000) {
-            bot.arm.setPosition(-340);
-            bot.slides.setPosition(0, 0.5);
-        }
-        bot.claw.open();
-        waitSeconds(1);
+            bot.setRobotPosition(-370, 1600, 0.5);
 
+            bot.setArmPos(-460);
 
-        startTime = System.currentTimeMillis();
-        while(System.currentTimeMillis() - startTime < 2 * 1000) {
-            bot.arm.setPosition(-380);
-            bot.slides.setPosition(0, 0.5);
-        }
+            //Pull down block
+            bot.setSlidePos(250, 0.5);
 
-        waitSeconds(0.5);
+            bot.claw.open();
 
-        bot.drive.toVectorOld(0, -500);
+            bot.waitSeconds(0.6);
 
-        while(bot.arm.setPosition(0) || bot.slides.setPosition(0, 0.5));
+        //SECOND BLOCK
+            bot.setArmPos(-380);
 
-        waitSeconds(0.5);
+            bot.waitSeconds(0.6);
 
-        bot.drive.toVectorOld(2400, 0);
+            //Drive backwards away from bar
+                bot.drive.toVectorOld(0, -200);
 
-        waitSeconds(1);
+            bot.setArmPos(0);
 
-        bot.drive.toVectorOld(0, -250);
+            bot.waitSeconds(0.6);
 
+            //Turn 90 deg clockwise
+                bot.drive.turn(220, 0.5);
 
-        stop();
+            bot.waitSeconds(0.6);
+
+            //Drive forward to sample area
+                bot.drive.toVectorOld(0, 1800);
+
+            bot.waitSeconds(0.5);
+
+            //Turn clockwise to human player
+                bot.drive.turn(240, 0.5);
+
+            bot.waitSeconds(0.5);
+
+            //Set arm in position to add block
+                bot.setArmPos(-930);
+
+            bot.waitSeconds(0.7);
+
+            //Grab second block
+                bot.setSlidePos(2000, 0.5);
+
+                bot.waitSeconds(1.0);
+
+                bot.claw.close();
+
+                bot.waitSeconds(0.5);
+
+                bot.setArmPos(-750);
+
+                bot.setSlidePos(0, 0.6);
+
+                bot.setArmPos(0);
+            //Drive to score second sample
+                bot.waitSeconds(0.5);
+
+                bot.drive.toVectorOld(0, -50);
+
+                bot.waitSeconds(0.5);
+
+                //Turn right parallel to wall and sample hanging bars
+                    bot.drive.turn(220, 0.5);
+
+                bot.waitSeconds(0.5);
+
+                //Drive forward to the bars
+                    bot.drive.toVectorOld(0,1800);
+
+                bot.waitSeconds(0.5);
+
+                //Turn right to face bars
+                    bot.drive.turn(250, 0.5);
+
+                bot.waitSeconds(0.5);
+
+                //Drive up to bars
+                    bot.drive.toVectorOld(0,750);
+
+                bot.waitSeconds(0.5);
+
+                //DELETE ( back up after running into bars
+                    bot.drive.toVectorOld(0, -100);
+
+                bot.waitSeconds(1);
+
+                //Raise arm up to bar
+                    bot.setRobotPosition(-370, 1800, 0.5);
+
+                //Hang sample
+                    bot.setArmPos(-440);
+
+                    bot.setSlidePos(150, 0.5);
+
+                    bot.claw.open();
+
+            bot.waitSeconds(0.6);
+
+            bot.setArmPos(-380);
+
+            bot.waitSeconds(0.6);
+
+            //PARKING
+                bot.drive.toVectorOld(0, -200);
+
+            stop();
     }
 }
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opModes/auto/OldAuto.java
@@ -0,0 +1,80 @@
+package org.firstinspires.ftc.teamcode.opModes.auto;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+import org.firstinspires.ftc.teamcode.subsystems.Robot;
+
+@Autonomous(name = "MainAuto")
+public class OldAuto extends LinearOpMode{
+    public void waitSeconds(double seconds) {
+        long startTime = System.currentTimeMillis();
+        while(true)
+            if (System.currentTimeMillis() - startTime >= seconds * 1000)
+                return;
+    }
+
+    public void runOpMode() throws InterruptedException {
+        Robot bot = new Robot(hardwareMap, telemetry);
+        //Drive drive = new Drive(hardwareMap, telemetry);
+        while(!isStarted()) {
+            bot.arm.setPosition(-750);
+        }
+        long startTime = System.currentTimeMillis();
+        while(System.currentTimeMillis() - startTime < 1.5 * 1000) {
+            bot.arm.setPosition(-400);
+        }
+
+        bot.drive.toVectorOld(0, 900);
+
+
+        waitSeconds(1);
+
+        startTime = System.currentTimeMillis();
+        while(System.currentTimeMillis() - startTime < 3 * 1000) {
+            bot.arm.setPosition(-280);
+            bot.slides.setPosition(950, 0.5);
+        }    
+        /*startTime = System.currentTimeMillis();
+        while(System.currentTimeMillis() - startTime < 2 * 1000) {
+            bot.arm.setPosition(1000);
+            bot.slides.setPosition(700, 0.5);]
+        }*/
+        startTime = System.currentTimeMillis();
+        while(System.currentTimeMillis() - startTime < 1 * 1000) {
+            bot.arm.setPosition(-340);
+            bot.slides.setPosition(950, 0.5);
+        }
+        startTime = System.currentTimeMillis();
+        while(System.currentTimeMillis() - startTime < 2 * 1000) {
+            bot.arm.setPosition(-340);
+            bot.slides.setPosition(0, 0.5);
+        }
+        bot.claw.open();
+        waitSeconds(1);
+
+
+        startTime = System.currentTimeMillis();
+        while(System.currentTimeMillis() - startTime < 2 * 1000) {
+            bot.arm.setPosition(-380);
+            bot.slides.setPosition(0, 0.5);
+        }
+
+        waitSeconds(0.5);
+
+        bot.drive.toVectorOld(0, -500);
+
+        while(bot.arm.setPosition(0) || bot.slides.setPosition(0, 0.5));
+
+        waitSeconds(0.5);
+
+        bot.drive.toVectorOld(2400, 0);
+
+        waitSeconds(1);
+
+        bot.drive.toVectorOld(0, -250);
+
+
+        stop();
+    }
+}
